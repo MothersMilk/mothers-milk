cbb6764c2404524ee447f3233688a7e1a9eb9439
Robin Miller <rmillerpdx@gmail.com>
2018-01-03 14:08:51 -0800

    add logout constants to dropsites and users reducers

--- src/dropSites/constants.js
+++ src/dropSites/constants.js
@@ -2,3 +2,4 @@ export const DROPSITES_LOAD = 'DROPSITES_LOAD';
 export const DROPSITE_ADD = 'DROPSITE_ADD';
 export const DROPSITE_UPDATE = 'DROPSITE_UPDATE';
 export const DROPSITE_DELETE = 'DROPSITE_DELETE';
+export const LOGOUT = 'LOGOUT';
