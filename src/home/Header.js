30f06140f58269e3414d74cf0d7a03ee56bb4d55
Robin Miller <rmillerpdx@gmail.com>
2018-02-05 14:53:52 -0800

    refactor Header.js

--- src/home/Header.js
+++ src/home/Header.js
@@ -5,10 +5,29 @@ import { NavLink, Link } from 'react-router-dom';
 
 class Header extends Component {
 
+  render(){
+    const { user , signin, signout } = this.props;    
+    return (
+      <header className="columns header">
+        <div className="column is-3 is-offset-1">
+          <Link to="/"><img alt="logo" src="/images/logo.jpg"/></Link>
+        </div>       
+        <div className="column is-2 is-offset-5">
+          { user ? <SignOut onSignOut={signout}/> : <SignIn onSignIn={signin}/> }
+        </div>
+      </header>
+    );
+  }
+}
+
+const SignOut = ({ onSignOut }) => <NavLink className="button" to="/" onClick={onSignOut}>Logout</NavLink>;
+
+class SignIn extends Component {
+
   handleSignIn = event => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    this.props.signin(
+    this.props.onSignIn(
       { 
         email: email.value,
         password: password.value 
@@ -16,36 +35,26 @@ class Header extends Component {
   }
 
   render(){
-    const { user , signout } = this.props;    
     return (
-      <header className="columns header">
-        <div className="column is-3 is-offset-1">
-          <Link to="/"><img alt="logo" src="/images/logo.jpg"/></Link>
-        </div>       
-        <div className="column is-2 is-offset-5">
-          <form className="field signInForm" onSubmit={event => this.handleSignIn(event)}>
-            {!user && <div>
-              <div className="control">
-                <input className="input"  placeholder="email" name="email"/>
-              </div>
-              <div className="control">
-                <input className="input" type="password" placeholder="password" name="password"/>
-              </div>
-              <div className="control">
-                <input className="button" type="submit"/>
-              </div>
-            </div>}
-            {user  && <NavLink className="button" to="/" onClick={signout}>Logout</NavLink>}
-          </form>
+      <form className="field signInForm" onSubmit={this.handleSignIn}>
+        <div>
+          <div className="control">
+            <input className="input"  placeholder="email" name="email"/>
+          </div>
+          <div className="control">
+            <input className="input" type="password" placeholder="password" name="password"/>
+          </div>
+          <div className="control">
+            <input className="button" type="submit"/>
+          </div>
         </div>
-      </header>
+      </form>
+       
     );
   }
 }
 
-export default connect(({ auth }) => ({
-  error: auth.error,
-  user: auth.user
-}),
-{ signin, signout }
+export default connect(
+  ({ auth }) => ({ user: auth.user }),
+  { signin, signout }
 )(Header);
