7a8e508e9f0840f3db8108214c7c2d8c64c12358
Robin Miller <rmillerpdx@gmail.com>
2018-02-08 17:29:32 -0800

    display mmbId in donation table

--- src/donations/AllDonations.js
+++ src/donations/AllDonations.js
@@ -23,6 +23,13 @@ class AllDonations extends PureComponent {
 
   render() {
     const { donations, staffView } = this.props;
+
+    function convertDate(inputFormat) {
+      function pad(s) { return (s < 10) ? '0' + s : s; }
+      const d = new Date(inputFormat);
+      return [pad(d.getMonth()+1), pad(d.getDate()), d.getFullYear()].join('/');
+    }
+
     const tableData = donations.length ? donations.map(item => {
       const { _id: id, donor, dropSite, quantity, status, date, mmbId } = item;
       const editing = this.state.editing === id ? true : false;
@@ -30,11 +37,6 @@ class AllDonations extends PureComponent {
       const currentStatusIndex = statusOptions.findIndex(status => status === item.status);
       const options = statusOptions.map((status, i) => i === currentStatusIndex ? <option key={i} value={status}>{status}</option> : <option key={i} value={status}>{status}</option>);
       
-      function convertDate(inputFormat) {
-        function pad(s) { return (s < 10) ? '0' + s : s; }
-        const d = new Date(inputFormat);
-        return [pad(d.getMonth()+1), pad(d.getDate()), d.getFullYear()].join('/');
-      }
 
       return (
         <tr className={editing ? 'animated fadeIn' : null} key={id}>
@@ -42,7 +44,7 @@ class AllDonations extends PureComponent {
             {date ? convertDate(date) : null}
           </td>
           <td>
-            {mmbId ? mmbId.name : null}
+            {mmbId ? mmbId : null}
           </td>
           <td>
             {donor ? donor.name : null}
@@ -90,6 +92,7 @@ class AllDonations extends PureComponent {
           <thead>
             <tr>
               <th>Date</th>
+              <th>MMB ID#</th>
               <th>Donor</th>
               <th>Drop Site</th>
               <th>Quantity</th>
