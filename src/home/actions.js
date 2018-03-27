a98540045187536938b4b5ee0fa372d47e63f84f
Robin Miller <rmillerpdx@gmail.com>
2018-03-27 13:54:04 -0700

    revert dispatch promise in getUser function

--- src/home/actions.js
+++ src/home/actions.js
@@ -17,10 +17,6 @@ export function checkForToken() {
     dispatch({
       type: actions.FETCHED_USER,
       payload: authApi.verify().then(id => authApi.getUser(id))
-        .then(dog => {
-          console.log('got user', dog.myDropSite);
-          return dog;
-        })
     });
   };
 }
