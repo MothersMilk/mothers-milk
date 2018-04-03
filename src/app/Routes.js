ce212146edbfdb156c910d8513de825ad900d368
Robin Miller <rmillerpdx@gmail.com>
2018-04-03 11:08:12 -0700

    remove unnecessary routes

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -4,18 +4,11 @@ import {
   Switch,
   Redirect
 } from 'react-router-dom';
-import DonorView from '../donor/DonorView';
 import Home from '../home/Home';
-import Admin from '../admin/Admin';
-import Staff from '../staff/Staff';
-import PrivateRoute from './PrivateRoute';
 
 export default () => (
   <Switch> 
     <Route exact path="/" render={() => <Home/>}/>
-    <PrivateRoute exact path="/home" component={DonorView}/>;
-    <PrivateRoute path="/admin" component={Admin}/>;
-    <PrivateRoute path="/staff" component={Staff}/>;
     <Redirect to="/"/>
   </Switch>  
 );
